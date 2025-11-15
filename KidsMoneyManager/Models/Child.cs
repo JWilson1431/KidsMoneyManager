0d7a5f70308a88436154c5504101ae473540b6eb
jWilson1431 <jwilson1431@gmail.com>
2025-11-15 18:53:30 -0500

    Add Child class

---
+++ KidsMoneyManager/Models/Child.cs
@@ -0,0 +1,11 @@
+﻿namespace KidsMoneyManager.Models
+{
+    public class Child
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; } = string.Empty;
+        public string Avatar { get; set; } = string.Empty;
+        public decimal BankBalance { get; set; } = 0m;
+        public decimal InterestRate { get; set; } = 0.01m;
+    }
+}
