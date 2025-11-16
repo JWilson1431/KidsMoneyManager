e7adaebc85a21e72cea7345698da25352c9ed1b5
jWilson1431 <jwilson1431@gmail.com>
2025-11-15 19:02:43 -0500

    Add basic child service

---
+++ KidsMoneyManager/Services/ChildService.cs
@@ -0,0 +1,30 @@
+﻿using KidsMoneyManager.Models;
+
+namespace KidsMoneyManager.Services
+{
+    public class ChildService
+    {
+        private List<Child> _children = new List<Child>();
+
+        public List<Child> GetAllChildren()
+        {
+            return _children;
+        }
+
+        public void AddChild(Child child)
+        {
+            _children.Add(child);
+        }
+
+        public void RemoveChild(Child child)
+        {
+            _children.Remove(child);
+        }
+
+        public Child? GetChildById(int id)
+        {
+            var child = _children.Where(x => x.Id == id).FirstOrDefault();
+            return child;
+        }
+    }
+}
